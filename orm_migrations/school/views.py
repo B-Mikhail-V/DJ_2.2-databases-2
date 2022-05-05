ca92726be3f32adc8ada33b11a905fd9badd453c
Mikail Bakulin <bakulin.mv@gmail.com>
2022-05-05 17:40:05 +0300

    2222

--- orm_migrations/school/views.py
+++ orm_migrations/school/views.py
@@ -1,14 +1,17 @@
 from django.views.generic import ListView
 from django.shortcuts import render
 
-from .models import Student
+from .models import Student, Teacher
 
 
 def students_list(request):
     template = 'school/students_list.html'
-    object_list = Student.objects.all()
+    object_list = Student.objects.order_by('name')
+    teachers_list = Teacher.objects.all()
+
     context = {
-        object_list: object_list,
+        'object_list': object_list,
+        'teachers_list': teachers_list,
 
     }
 
