07ed32b70f3e3254b21db1eed5b8d3e90073bea9
B-Mikhail-V <bakulin.mv@gmail.com>
2022-05-11 21:37:22 +0300

    4444

--- m2m-relations/website/settings.py
+++ m2m-relations/website/settings.py
@@ -78,6 +78,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_m2m_relations',
         'HOST': '127.0.0.1',
+        'USER': 'postgres',
+        'PASSWORD': 'postpsw',
         'PORT': '5432',
     }
 }
