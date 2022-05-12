f44b80c67b3f0f0480a4aa26f6d09f20edfe4b9e
Mikail Bakulin <bakulin.mv@gmail.com>
2022-05-12 10:11:51 +0300

    5555

--- m2m-relations/articles/admin.py
+++ m2m-relations/articles/admin.py
@@ -1,8 +1,34 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Tag, Scope
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        count = 0
+        for form in self.forms:
+            if form.cleaned_data.get('is_main', False) and not self._should_delete_form(form):
+                count += 1
+        if count > 1:
+            raise ValidationError('Только один тэг может быть основным')
+        if count == 0:
+            raise ValidationError('Обязательно укажите основной тэг')
+        return super().clean()  # вызываем базовый код переопределяемого метода
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
+    extra = 0
 
 
 @admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ScopeInline]
+
+
+
+@admin.register(Tag)
 class ArticleAdmin(admin.ModelAdmin):
     pass
--- m2m-relations/articles/models.py
+++ m2m-relations/articles/models.py
@@ -11,6 +11,7 @@ class Article(models.Model):
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
 
     def __str__(self):
         return self.title
@@ -19,7 +20,17 @@ class Tag(models.Model):  # Создаем класс для тэгов
     name = models.CharField(max_length=32)
     article = models.ManyToManyField(Article, related_name='tags', through='Scope')
 
-class Scope(models.Model):  № Это промежуточная таблица, в которой 5 свойтсва один ко многим + is_main - главный тэг
+
+
+    def __str__(self):
+        return self.name
+
+class Scope(models.Model):  # Это промежуточная таблица, в которой 5 свойтсва один ко многим + is_main - главный тэг
     article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scopes')
     is_main = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'Тема'
+        verbose_name_plural = 'Темы'
+        ordering = ['-is_main']
